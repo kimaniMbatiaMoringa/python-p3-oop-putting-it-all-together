d39ad33d88b7c494f4080c06ebe2d46a4abd868a
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-09-01 23:32:41 +0300

    Assignment completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self,title,page_count):
+        self.title = title
+        self.page_count = page_count
         
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self,value):
+        if type(value) != int:
+            print("page_count must be an integer")
+        else:
+            self._page_count = value    
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    page_count = property(fget=get_page_count,fset=set_page_count)    
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
-
+import ipdb
 class Shoe:
-    pass
+    def __init__(self,brand,size,condition="Old"):
+        self.brand = brand
+        self.size = size
+        self.conditon = condition
+    
+    def get_shoe_size(self):
+        return self._size
+    
+    def set_shoe_size(self,value):
+        if type(value)!= int:
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self, condition="New"):
+        print("Your shoe is as good as new!")
+        self.conditon = condition
+
+    size = property(fget=get_shoe_size,fset=set_shoe_size)
+
+ipdb.set_trace()  
